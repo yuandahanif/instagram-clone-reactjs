5a65ca5cf315209f607ab6ae71cea1d56412bf1e
Yuanda <kirishima699@gmail.com>
2020-08-24 23:20:30 +0700

    Updates

---
+++ precache-manifest.4d02def5285c89b797ff4546b2c67340.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37538e3ecadfd98fa7d6051c591e9be4",
+    "url": "/instagram-clone-reactjs/index.html"
+  },
+  {
+    "revision": "daf496c3d14131f7dc04",
+    "url": "/instagram-clone-reactjs/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "ce372c55f0b30e6192d4",
+    "url": "/instagram-clone-reactjs/static/css/main.45f9d219.chunk.css"
+  },
+  {
+    "revision": "daf496c3d14131f7dc04",
+    "url": "/instagram-clone-reactjs/static/js/2.cb663d79.chunk.js"
+  },
+  {
+    "revision": "241e2720c0228744c712295a835ee806",
+    "url": "/instagram-clone-reactjs/static/js/2.cb663d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce372c55f0b30e6192d4",
+    "url": "/instagram-clone-reactjs/static/js/main.1d10ddd2.chunk.js"
+  },
+  {
+    "revision": "1132deaf71e297df2292",
+    "url": "/instagram-clone-reactjs/static/js/runtime-main.53e0b9d5.js"
+  },
+  {
+    "revision": "a197a8457c431b63603b538d5f15433f",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Black.a197a845.woff"
+  },
+  {
+    "revision": "bc6ccb1e7ad2c79bd390cafb923db667",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Black.bc6ccb1e.woff2"
+  },
+  {
+    "revision": "7786e3f7535ac6e4b7a46edfe14571af",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-BlackItalic.7786e3f7.woff"
+  },
+  {
+    "revision": "deb68985229b53325077100f30982cec",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-BlackItalic.deb68985.woff2"
+  },
+  {
+    "revision": "04822f5127a9ff271db76fdd6986cb10",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Bold.04822f51.woff2"
+  },
+  {
+    "revision": "5a561b1b25f9714c1cd125911694cc62",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Bold.5a561b1b.woff"
+  },
+  {
+    "revision": "1d45846d7c095eaca2fed43f71f9eeca",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-BoldItalic.1d45846d.woff"
+  },
+  {
+    "revision": "7c9bb4af2dd759c31dc240a1f774a8e8",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-BoldItalic.7c9bb4af.woff2"
+  },
+  {
+    "revision": "337027b064adec6ef104f189a73c5f97",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Italic.337027b0.woff2"
+  },
+  {
+    "revision": "790584c34d7e0f9f857574f1e0eaed95",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Italic.790584c3.woff"
+  },
+  {
+    "revision": "0b1bf7ea63f86f5f0e885b55a4c3cd23",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Light.0b1bf7ea.woff2"
+  },
+  {
+    "revision": "c707e69bc8faaa932609a4b9e1a2a1c1",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Light.c707e69b.woff"
+  },
+  {
+    "revision": "46292100b8244959afb5d8f0adbcee9f",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-LightItalic.46292100.woff"
+  },
+  {
+    "revision": "8cf414dba05177704b9442e5a3bd9827",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-LightItalic.8cf414db.woff2"
+  },
+  {
+    "revision": "307e4a7088582df1deafb1bab70b5b7d",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Medium.307e4a70.woff2"
+  },
+  {
+    "revision": "9e8e3279709c2cdd415f063205f39e06",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Medium.9e8e3279.woff"
+  },
+  {
+    "revision": "1ee76fd55271e9dd08c0096b1869da4e",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-MediumItalic.1ee76fd5.woff2"
+  },
+  {
+    "revision": "7aa8d1bfd55967ebd5e5a8b984209690",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-MediumItalic.7aa8d1bf.woff"
+  },
+  {
+    "revision": "04ce8adf9a7d2febe5ecad8cb33d2446",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Regular.04ce8adf.woff2"
+  },
+  {
+    "revision": "bdadb5127b682d4d46cb2b589497a8fb",
+    "url": "/instagram-clone-reactjs/static/media/Rubik-Regular.bdadb512.woff"
+  }
+]);
